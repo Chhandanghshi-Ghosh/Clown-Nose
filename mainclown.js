883d8c5323ef0424130a8ef067fbf948bbc010a3
Chhandanghshi-Ghosh <73993378+Chhandanghshi-Ghosh@users.noreply.github.com>
2021-02-02 15:54:22 +0530

    Add files via upload

--- mainclown.js
+++ mainclown.js
@@ -1,35 +1,42 @@
-function preload(){
-clown_nose = loadImage("https://i.postimg.cc/FR4hxsfb/clown-nose.jpg")
-}
-function setup(){
-    canvas = createCanvas(300,300)
-    canvas.center()
-    video = createCapture(VIDEO)
-    video.size(300,300)
-    video.hide()
-    poseNet = ml5.poseNet(video,modelLoaded)
-    poseNet.on('pose',gotposes)
-}
-function modelLoaded(){
-    console.log("poseNet model loaded!!")
+noseX=0;
+noseY=0;
+
+function preload() {
+  clown_image=loadImage("https://i.postimg.cc/MGrvxNSN/clownnose.png")
 }
 
-function takeSnapshot(){
-    save("clown_nose_filter.jpg")
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(300, 300);
+  video.hide();
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
-function gotposes(results){
- 
-if(results.length > 0){
-    console.log(results)
+function modelLoaded() {
+  console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
     noseX = results[0].pose.nose.x;
     noseY = results[0].pose.nose.y;
-    console.log("noseX" + noseX)
+    console.log("nose x = " + noseX);
+    console.log("nose y = " + noseY);
+  }
+}
+function draw() {
+  image(video, 0, 0, 300, 300);
+  image(clown_image,noseX-18,noseY-20,30,30)
+  
 }
 
-
+function take_snapshot(){    
+  save('clown_nose_image.png');
 }
-function draw(){
-    image(video,0,0,300,300);
-    image(clown_nose,noseX-18,noseY-20,30,30)
-    }
